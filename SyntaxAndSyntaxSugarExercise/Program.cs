da51c869dde7338e4df86fb1f0b7cd01d04c0e3d
fred47 <Frdrck.hart@gmail.com>
2023-02-28 04:42:26 -0500

    Finished Syntax and Syntax Sugar

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            //inferred typing
+
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+
+               
+            //}
+
+
+            // ternary operator
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+
         }
     }
 }
